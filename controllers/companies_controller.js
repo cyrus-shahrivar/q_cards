1d104ed99519236df790ed93f52b4af02b626a96
shishkina <shishkina.kate@gmail.com>
2015-11-21 15:06:29 -0500

    added controllers, helpers, minor modifications to models

---
+++ controllers/companies_controller.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose'),
+    Company = require('../models/company.js');
+
+
+module.exports.controller = function(app){
+  app.get('/companies', function(req, res){
+    Company.find().exec(function(err, companies){
+      if(err) return next(err);
+      return.send(companies);
+    });
+  });
+  
+
+}
---
+++ controllers/users_controller.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose'),
+    User = require('../models/user.js');
+
+module.exports.controller = function(app){
+      app.get('/contacts', function(req, res){
+        User.find().exec(function(err, contacts){
+          if(err) return next(err);
+          res.send(contacts);
+        });
+      });
+      //   "/user" is the current user, right? So when I try to add a new card, it will be added to the current user id
+      //therefore, do I need to specify the id?
+      app.post('/user/:id/newContact', function(req, res){
+        var current_user = User.findById(req.params.id);
+        
+          })
+        })
+      })
+
+
+}
