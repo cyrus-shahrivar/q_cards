37728929b84f94a6585ea509856025b79b3a5524
shishkina <shishkina.kate@gmail.com>
2015-11-23 11:59:19 -0500

    dealing with merge conflict

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -5,9 +5,43 @@ var mongoose = require('mongoose'),
     // Sessions = require('../models/session.js');
 
 
-module.exports.controller = function(app){
+var isAuthenticated = function(req, res, next){
+	if(req.isAuthenticated()) return next();
 
+	res.redirect('/');
+}
 
+module.exports.controller = function(passport){
+		//GET login page
+		router.get('/', function(req, res){
+			res.render('index', {message: req.flash('Please, log in')});
+		});
+		//Login POST
+		router.post('/login', passport.authenticate('login',{
+			successRedirect: '/home',
+			failureRedirect: '/',
+			failureFlash: true
+		}));
+		//GET registration page
+		router.get('/signup', function(req, res){
+				res.render('register', {message: req.flash('message')});
+		});
+		//POST to registration
+		router.post('/signup', passport.authenticate('signup', {
+			successRedirect: '/home',
+			failureRedirect: '/signup',
+			failureFlash: true
+		}));
+		//GET Home page
+		router.get('/home', isAuthenticated, function(req, res){
+			res.render('home', {user: req.user});
+		});
+
+		//LOG OUT
+		router.get('/signout', function(req, res){
+			req.logout();
+			res.redirect('/');
+		});
 };
 
 module.exports = router;
