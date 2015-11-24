95c62ec88b4d5e8757d224140324aa1abb1e7b92
Cyrus Shahrivar <cyrus.shahrivar@gmail.com>
2015-11-24 11:44:41 -0500

    we got several routes working

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express'),
     logger = require('morgan'),
-    session = require('express-session'),
-    passport = require('passport'),
+    // session = require('express-session'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
-    bcrypt = require('bcrypt'),
-    router = express.Router(),
+    // bcrypt = require('bcrypt'),
+    // router = express.Router(),
     fs = require('fs'),
+    // User = require('./models/user.js'),
     companies = require('./controllers/companies_controller.js'),
     users =  require('./controllers/users_controller.js'),
-    sessions = require('./controllers/sessions_controller.js'),
+    // sessions = require('./controllers/sessions_controller.js'),
     app = express();
 
 //set up middleware
@@ -17,21 +17,18 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
-app.use('/bower_components/', express.static(__dirname + '/bower_components/'));
-app.use(session);
-// secret saveUninitialized, etc...
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Initialize Passport
-var initPassport = require('./passport/init');
-initPassport(passport);
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+// app.use(session({
+//   secret: "my favorite pillow",
+//   saveUninitialized: false,
+//   resave: false
+// }));
 
 //connect to mongo database called qcards_app
 mongoose.connect('mongodb://localhost/q_cards_app', function (err) {
   if(err){
     console.log(err);
-  }else {
+  } else {
     console.log('connection successful');
   }
 });
@@ -39,19 +36,7 @@ mongoose.connect('mongodb://localhost/q_cards_app', function (err) {
 app.listen(3000, function(){
   console.log('Listening to a frilled lizard on port 3000..');
 });
-//this command will tell javascript to go in order.
-//And if the file substring is javascirpt, it will route us there and use that file as a controller.
-
 
 // app.use('/', sessions);
-app.use('/', users);
-app.use('/', companies);
-
-// THIS IS CAUSING A BUG. FOR SOME REASON THE SERVER ISN'T FINDING './controllers/:file'
-// fs.readdirSync('./controllers').forEach(function(file) {
-//   if (file.substr(-3) == '.js') {
-//     route = require('./controllers/' + file) (passport);
-//     route.controller(app);
-//   }
-// });
- 
+app.use('/users', users);
+app.use('/companies', companies);
